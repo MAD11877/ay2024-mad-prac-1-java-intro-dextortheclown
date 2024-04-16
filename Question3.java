57fc414e5dc32471686a675f2b6b52acb0d0eb67
dextortheclown <126008185+dextortheclown@users.noreply.github.com>
2024-04-16 13:25:38 +0800

    3 & 4

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.println("Please gimme a number ");
+    //System.out.println("Please gimme a number ");
     int num = in.nextInt();
     System.out.println(num*num);
   }
